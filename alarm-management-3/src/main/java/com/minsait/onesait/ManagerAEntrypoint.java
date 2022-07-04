e847bb9dc66411bbd93bea799e8d722bee11062b
Gomez Cornejo Gil, Francisco Javier <fjgcornejo@minsait.com>
2022-07-04 19:00:55 +0200

    changes

--- alarm-management-3/src/main/java/com/minsait/onesait/ManagerAEntrypoint.java
+++ alarm-management-3/src/main/java/com/minsait/onesait/ManagerAEntrypoint.java
@@ -8,8 +8,8 @@ import okhttp3.Response;
 
 public class ManagerAEntrypoint {
 
-	private static final String API_KEY = "49832a9c2d7645caa0ef497706edef34";
-	private static final String API_ENDPOINT ="https://onesait-telco-poc.onesaitplatform.com/api-manager/server/api/v1/ticket-management/manage/";
+	private static final String API_KEY = "526a85610da04ecd94b1e2b00d0116a7";
+	private static final String API_ENDPOINT ="https://development.onesaitplatform.com/api-manager/server/api/v1/telco-proxy/proxy-ticket/";
 	private static final String AGENT="Agent_C";
 
 	public String handleRequest(String id) {
@@ -20,7 +20,7 @@ public class ManagerAEntrypoint {
 			final Random r = new Random();
 			final long sleep = x+(long)(r.nextDouble()*(y-x));
 			Thread.sleep(sleep);
-			final Request request = new Request.Builder().url(API_ENDPOINT + AGENT + "/" +id).get()
+			final Request request = new Request.Builder().url(API_ENDPOINT + "?agent=" +AGENT + "&ticketId=" +id).get()
 					.build();
 			final Response response = clientHttp().newCall(request).execute();
 			return "{\"status\":\""+result + response.body().string()+"\"}";
